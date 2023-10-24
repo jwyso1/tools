1345590cd2837eae6f3f788eb6a8f20df2c19aa6
jwysokinski <v-jwysokinski@expediagroup.com>
2023-10-24 15:25:22 +0200

    LoggingStream

--- constants/logging.py
+++ constants/logging.py
@@ -1,3 +1,11 @@
-from typing import Final
+from typing import Final, Protocol
 
 DEFAULT_FORMATTER: Final[str] = '%(asctime)s - %(message)s'
+
+class LoggingStream(Protocol):
+    def flush(self):
+        pass
+    def write(self):
+        pass
+    def close(self):
+        pass
